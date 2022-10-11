55a46826cc05dfac7719d3023133f5c7640d94b8
shion0625 <xkaito0912@gmail.com>
2022-10-11 10:38:35 +0900

    dbとの接続を別ファイルに書き出し

--- server.go
+++ server.go
@@ -1,36 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"log"
 	"net/http"
-	"os"
-	"github.com/joho/godotenv"
-  "gorm.io/driver/postgres"
-  "gorm.io/gorm"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/shion0625/my-portfolio-backend/graph"
 	"github.com/shion0625/my-portfolio-backend/graph/generated"
+	"github.com/shion0625/my-portfolio-backend/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 
 func main() {
-	loadEnv()
-	//データベースへの接続
-	dsn := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+	db := db.ConnectGORM()
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	e.GET("/", welcome())
+	e.GET("/playground", pagePlayground)
 
 	graphqlHandler := handler.NewDefaultServer(
 		generated.NewExecutableSchema(
@@ -42,9 +34,6 @@ func main() {
 		return nil
 	})
 
-	e.GET("/", welcome())
-	e.GET("/playground", pagePlayground)
-
 	port := os.Getenv("PORT")
 	errPort := e.Start(port)
 	if errPort == nil {
@@ -55,18 +44,6 @@ func main() {
 func welcome() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return c.String(http.StatusOK, "Welcome!")
-	}
-}
-
-func loadEnv() {
-	// ここで.envファイル全体を読み込みます。
-	// この読み込み処理がないと、個々の環境変数が取得出来ません。
-	// 読み込めなかったら err にエラーが入ります。
-	err := godotenv.Load(".env")
-
-	// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
-	if err != nil {
-		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
 }
 
