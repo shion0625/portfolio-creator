e36e3ea3d3cc3f76bd5a6ca52f2447c153165a14
shion0625 <xkaito0912@gmail.com>
2022-11-20 10:48:47 +0900

    古いコードをコメントアウト

--- service/user.go
+++ service/user.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"github.com/shion0625/my-portfolio-backend/graph/model"
 	"github.com/shion0625/my-portfolio-backend/db"
-	"gorm.io/gorm"
-	"github.com/vektah/gqlparser/v2/gqlerror"
+	// "gorm.io/gorm"
+	// "github.com/vektah/gqlparser/v2/gqlerror"
 
 )
 
@@ -29,31 +29,31 @@ func UserGetByEmail(ctx context.Context, email string) (*model.User, error) {
 }
 
 
-func UserUpdateByID(ctx context.Context, input model.UpdateUserInput) (*model.User, error){
-	db := db.ConnectGORM()
-	user, err :=UserGetByID(ctx, input.ID)
-	if err != nil {
-		// if user not found
-		if err == gorm.ErrRecordNotFound {
-			return nil, &gqlerror.Error{
-				Message: "id not found",
-			}
-		}
-		return nil, err
-	}
+// func UserUpdateByID(ctx context.Context, input model.UpdateProfileInput) (*model.User, error){
+// 	db := db.ConnectGORM()
+// 	user, err :=UserGetByID(ctx, input.ID)
+// 	if err != nil {
+// 		// if user not found
+// 		if err == gorm.ErrRecordNotFound {
+// 			return nil, &gqlerror.Error{
+// 				Message: "id not found",
+// 			}
+// 		}
+// 		return nil, err
+// 	}
 
-	if input.Name == nil {
-		input.Name = user.Name
-	}
-	if input.Email == nil {
-		input.Email = user.Email
-	}
+// 	if input.Name == nil {
+// 		input.Name = user.Name
+// 	}
+// 	if input.Email == nil {
+// 		input.Email = user.Email
+// 	}
 
-	if err := db.Model(&user).Updates(model.User{Name: input.Name, Email: input.Email}).Error; err != nil {
-		return nil, err
-	}
-	if err := db.Save(&user).Error; err != nil {
-		return nil, err
-	}
-	return user, nil
-}
+// 	if err := db.Model(&user).Updates(model.User{Name: input.Name, Email: input.Email}).Error; err != nil {
+// 		return nil, err
+// 	}
+// 	if err := db.Save(&user).Error; err != nil {
+// 		return nil, err
+// 	}
+// 	return user, nil
+// }
