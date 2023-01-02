b4f6ec24126455d23a57c139126af0214ef4a924
shion0625 <xkaito0912@gmail.com>
2023-01-02 14:10:07 +0900

    不必要なコードの削除

--- backend/graph/resolver/user.resolvers.go
+++ backend/graph/resolver/user.resolvers.go
@@ -38,13 +38,3 @@ func (r *userResolver) Profile(ctx context.Context, obj *model.User) (*model.Pro
 func (r *Resolver) User() generated.UserResolver { return &userResolver{r} }
 
 type userResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *userResolver) EmailVerified(ctx context.Context, obj *model.User) ([]*string, error) {
-	panic(fmt.Errorf("not implemented: EmailVerified - emailVerified"))
-}
