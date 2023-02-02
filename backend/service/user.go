2f1ea3b6bf5787ceeae9f34f042eafd9e2358afb
shion0625 <xkaito0912@gmail.com>
2023-02-02 20:43:24 +0900

    userのデータベース処理を分離

--- backend/service/user.go
+++ backend/service/user.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"github.com/shion0625/portfolio-creater/backend/graph/model"
 	"github.com/shion0625/portfolio-creater/backend/db"
-	// "gorm.io/gorm"
+	"gorm.io/gorm"
 	// "github.com/vektah/gqlparser/v2/gqlerror"
 
 )
 
-func UserGetByID(ctx context.Context, id string) (*model.User, error) {
-	db := db.ConnectGORM()
+func UserGetByID(ctx context.Context, db *gorm.DB, id string) (*model.User, error) {
 	var user model.User
-	if err := db.Table("users").Where("id = ?", id).Take(&user).Error; err != nil {
+	if err := db.Where("id = ?", id).Find(&user).Error; err != nil {
 		return nil, err
 	}
 
@@ -28,7 +27,23 @@ func UserGetByEmail(ctx context.Context, email string) (*model.User, error) {
 	return &user, nil
 }
 
+func UserTotalCount(ctx context.Context,db *gorm.DB) (int64, error) {
+	var totalCount int64
+	if err := db.Model(&model.User{}).Count(&totalCount).Error; err != nil {
+		return 0, err
+	}
+	return totalCount, nil
+}
 
+func UsersGet(ctx context.Context, db *gorm.DB, limit int, offset int) ([]*model.User,int64, error) {
+	var users []*model.User
+	result := db.Limit(limit).Offset(offset).Find(&users)
+	if result.Error != nil {
+		return nil,0, result.Error
+	}
+
+	return users, result.RowsAffected, nil
+}
 // func UserUpdateByID(ctx context.Context, input model.UpdateProfileInput) (*model.User, error){
 // 	db := db.ConnectGORM()
 // 	user, err :=UserGetByID(ctx, input.ID)
