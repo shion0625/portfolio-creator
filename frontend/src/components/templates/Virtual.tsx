707daa06d6f13c009ed1fcb18df0563053a69ff3
shion0625 <xkaito0912@gmail.com>
2023-02-22 00:42:53 +0900

    スクロール位置でデータの取得の実装

--- frontend/src/components/templates/Virtual.tsx
+++ frontend/src/components/templates/Virtual.tsx
@@ -1,5 +1,5 @@
 import Grid from '@mui/material/Grid'
-import React, {useEffect} from 'react'
+import React, {useEffect, useCallback} from 'react'
 import { WorkImageCard } from '~/components/parts/WorkImageCard'
 import { Work } from '~/models/types'
 import { Virtualizer } from '@tanstack/react-virtual'
@@ -15,15 +15,41 @@ type Props = {
 export const Virtual: React.FC = (): JSX.Element => {
 
   const parentRef = React.useRef<Element>(null)
-  const { allData, onClick } = useGetMore()
+  const { works, onClick } = useGetMore()
 
     const rowVirtualizer = useVirtualizer({
-    count: allData.length + 1,
+    count: works.hasNextPage ? works.contents.length + 1 : works.contents.length,
     getScrollElement: () => parentRef.current,
     estimateSize: () => 100,
     overscan: 12,
-    debug: true
-  })
+    })
+
+  const onScroll = useCallback(() => {
+    const el = parentRef.current
+    if (el) {
+      const rate = window.pageYOffset / (el.scrollHeight - el.scrollTop)
+        // スクロール位置の割合が8割を超えている場合は描画するアイテムを追加
+      if (rate > 0.6) {
+        const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
+
+    if (!lastItem) {
+      return;
+    }
+
+    if (
+      lastItem.index >= works.contents.length - 1 &&
+      works.hasNextPage
+    ) {
+      onClick();
+    }
+      }
+    }
+  },[])
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   return (
     <Box component='main' sx={{ m: 2 }} ref={parentRef}>
@@ -31,8 +57,8 @@ export const Virtual: React.FC = (): JSX.Element => {
         height: `${rowVirtualizer.getTotalSize()}px`
       }}>
         {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-          const isLoaderRow = virtualRow.index > allData.length - 1
-          const post: Work = allData[virtualRow.index]
+          const isLoaderRow = virtualRow.index > works.contents.length - 1
+          const post: Work = works.contents[virtualRow.index]
           return (
             <>
               {isLoaderRow ?
@@ -46,8 +72,7 @@ export const Virtual: React.FC = (): JSX.Element => {
           )
         })}
         </Grid>
-        <button onClick={onClick}>もっと</button>　
-          {/* <CircularProgress /> */}
+        {/* <button onClick={onClick}>もっと</button>　 */}
     </Box>
   )
 }
--- frontend/src/components/views/Works/hooks.ts
+++ frontend/src/components/views/Works/hooks.ts
@@ -1,27 +1,34 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { Work } from '~/models/types'
-import { GetWorkNodesQuery, GetWorkNodesDocument } from '~/models/client'
-import { useLazyQuery,  } from '@apollo/client';
+import { Work, PaginationInfo } from '~/models/types'
+import { GetWorksQuery, GetWorksDocument } from '~/models/client'
+import { useLazyQuery  } from '@apollo/client';
 
 const useGetWorks = () => {
-  const [ getWorks,{ data, loading, error } ]= useLazyQuery<GetWorkNodesQuery>(GetWorkNodesDocument)
-  return {getWorks, worksData: data?.workNodes, loading, error}
+  const [ getWorks,{ data, loading, error } ]= useLazyQuery<GetWorksQuery>(GetWorksDocument)
+  return {getWorks, worksData: data?.works.nodes, loading, error}
 }
 
 type Last =
   | { order: "update", searched: string, num: number }
   | { order: "create", searched: string, num: number }
 
-  type State =
-  | { _tag: "init" }
-  | { _tag: "loading" }
-  | { _tag: "success", contents: Work[] }
+type Works = PaginationInfo & {
+  contents: Work[],
+}
 
 export const useGetMore = () =>
 {
   const DEFAULT_VOLUMES = Number(process.env.NEXT_PUBLIC_DEFAULT_VOLUMES)
   const CURRENT_TIME = new Date().toISOString().replace('T', ' ').replace('Z', '');
-  const [works, setWorks] = useState<Work[]>([])
+  const [works, setWorks] = useState<Works>({
+    count: 0,
+    hasNextPage: true,
+    hasPreviousPage: false,
+    page: 0,
+    paginationLength: 0,
+    totalCount: 0,
+    contents: []
+  })
   const [variable, setVariable] = useState<Last>({ order: "create", searched: CURRENT_TIME, num: 9999 })
   let { getWorks, error } = useGetWorks()
   let lastData: Work
@@ -30,20 +37,21 @@ export const useGetMore = () =>
     getWorks({
       variables: { limit: DEFAULT_VOLUMES, order: variable.order, searched: variable.searched, num: variable.num },
       onCompleted: (data) => {
-    setWorks((current) => [
-      ...current,
-      ...data.workNodes])
-        lastData = data.workNodes.slice(-1)[0]
+        setWorks({
+          ...works,
+          hasNextPage: data.works.pageInfo.hasNextPage,
+          contents: [...works.contents, ...data.works.nodes]
+    })
+        lastData = data.works.nodes.slice(-1)[0]
       }
     })
   }, [variable])
 
   const onClick = useCallback((): void => {
-    console.log(lastData)
     if (lastData) {
       setVariable({order: "create", searched: lastData.created_at, num: lastData.number_of_work })
     }
   }, [variable])
 
-  return {allData: works, onClick}
+  return {works: works, onClick}
 }
