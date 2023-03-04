45c6f001117a091e0f2584a800d4147a1d8425f6
shion0625 <xkaito0912@gmail.com>
2023-03-04 22:15:56 +0900

    キーワードが変わった際に再検索

--- frontend/src/components/templates/SearchWorks/hook/useFetchSearchWorks.ts
+++ frontend/src/components/templates/SearchWorks/hook/useFetchSearchWorks.ts
@@ -1,7 +1,6 @@
 import { useSearch, Variables, SearchDataState } from './useSearch'
-import { last } from 'lodash'
-import { useState, useEffect, useCallback, useRef } from 'react'
-import { Work, User, WorkPagination, UserPagination, Model, SortBy, Node } from '~/models/types'
+import { useState, useCallback, useRef } from 'react'
+import { Work, User, Model, SortBy, Node } from '~/models/types'
 import { useDidUpdateEffect } from '~/utils/hook/useDidUpdateEffect'
 
 type SearchNodeDataState<T extends Model> = T extends Model.Work ? Work : User
@@ -19,6 +18,7 @@ export const useFetchSearchWorks = <T extends Model>(
   sortBy: SortBy,
   keyword: string,
 ): UseFetchSearchWorks<T> => {
+  //初期データ
   const [searchData, setSearchData] = useState<SearchDataState<T>>(() => ({
     type: target,
     pageInfo: {
@@ -31,26 +31,52 @@ export const useFetchSearchWorks = <T extends Model>(
     },
     nodes: [],
   }))
-
+  //初期のuseSearchの引数
   const [variables, setVariable] = useState<Variables<T>>({
     target,
     keyword,
     sortBy,
     searchedAt: CURRENT_TIME,
     num: 9999,
-    limit: 20,
+    limit: DEFAULT_VOLUMES,
   })
+  //カーソルスクロール使用するための最後のデータ
   const lastDataRef = useRef<Node | undefined>(undefined)
 
-  const { searchResult, refetch } = useSearch(variables, setSearchData, lastDataRef)
+  const { refetch } = useSearch(variables, setSearchData, lastDataRef)
 
+  //keywordが変更になった際にデータを
   useDidUpdateEffect(() => {
+    setSearchData((prev) => ({
+      ...prev,
+      pageInfo: {
+        count: 0,
+        hasNextPage: true,
+        hasPreviousPage: false,
+        page: 0,
+        paginationLength: 0,
+        totalCount: 0,
+      },
+      nodes: [],
+    }))
+    setVariable((prev) => ({
+      ...prev,
+      keyword,
+      searchedAt: CURRENT_TIME,
+      num: 9999,
+      limit: DEFAULT_VOLUMES,
+    }))
+  }, [keyword])
+
+  //variablesが変更になった時だけrefetchが発火する
+  useDidUpdateEffect(() => {
+    //variablesを変更した後に発火し、更新された変数でデータの取得
     refetch({ variables })
   }, [variables])
 
+  //lastDataRef.currentをvariablesに渡す。
   const onScroll = useCallback((): void => {
     const lastData = lastDataRef.current as SearchNodeDataState<T>
-    console.log(lastData)
     if (lastData) {
       setVariable((prev) => ({
         ...prev,
--- frontend/src/components/templates/SearchWorks/index.tsx
+++ frontend/src/components/templates/SearchWorks/index.tsx
@@ -12,29 +12,8 @@ type Props = {
 
 const SearchWorks: React.FC<Props> = ({ keyword }) => {
   const { searchData, onScroll } = useFetchSearchWorks(Model.Work, SortBy.Update, String(keyword))
-  console.log(searchData)
-
-  // const { data, loading, error } = useQuery<SearchQuery>(SearchDocument, {
-  // variables: {
-  //   target: Model.Work,
-  //   limit: 10,
-  //   sortBy: SortBy.Update,
-  //   keyword,
-  //   num: 99999,
-  //   searchedAt: "2023-03-05 02:30:46.510146"
-  // }
-  // })
-  // const { search, searchResult } = useSearch({
-  //   target: Model.Work,
-  //   limit: 10,
-  //   sortBy: SortBy.Update,
-  //   keyword,
-  //   num: 99999,
-  //   searchedAt: "2023-03-05 02:30:46.510146"
-  // })
 
   return <WorksInfScroll pageInfo={searchData.pageInfo} works={searchData.nodes} onScroll={onScroll} />
-  return <></>
 }
 
 export default SearchWorks
