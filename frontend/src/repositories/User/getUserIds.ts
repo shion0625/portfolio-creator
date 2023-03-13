ddde3e71e497aebf3c3f7f7290e97e239de59d70
shion0625 <xkaito0912@gmail.com>
2023-03-13 17:45:53 +0900

    limitの個数減らす

--- frontend/src/repositories/User/getUserIds.ts
+++ frontend/src/repositories/User/getUserIds.ts
@@ -2,6 +2,6 @@ import { GetUserIdsQuery } from '~/models/client'
 import { executeQuery } from '~/utils/hook/useServerSideQuery'
 
 export async function getUserIds(limit: number, offset: number): Promise<GetUserIdsQuery> {
-  const users = executeQuery('GetUserIds', { limit, offset })
+  const users = executeQuery('GetUserIds', { limit:2, offset })
   return users
 }
