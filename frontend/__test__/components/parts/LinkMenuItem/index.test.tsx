8f9ba9678d44789d447bfa7ecdb274ef65eed4d7
shion0625 <xkaito0912@gmail.com>
2023-04-01 19:57:18 +0900

    LinkMenuItemの単体テスト実装

---
+++ frontend/__test__/components/parts/LinkMenuItem/index.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import LinkMenuItem from '~/components/parts/LinkMenuItem'
+
+describe('LinkMenuItem', () => {
+  const menuName = 'menu name'
+  const href = '/path/to/link'
+  const onClick = jest.fn()
+
+  it('renders the menu item with the provided props', () => {
+    render(<LinkMenuItem href={href} menuName={menuName} onClick={onClick} />)
+    const menuItem = screen.getByRole('menuitem')
+    expect(menuItem).toHaveTextContent(menuName)
+    expect(menuItem).toHaveAttribute('href', href)
+  })
+
+  it('calls the onClick callback when the menu item is clicked', () => {
+    render(<LinkMenuItem href={href} menuName={menuName} onClick={onClick} />)
+    const menuItem = screen.getByRole('menuitem')
+    menuItem.click()
+    expect(onClick).toHaveBeenCalledTimes(1)
+  })
+})
