37cf08e8fd3dbfd0162b1be4560da3224c58b29f
shion0625 <xkaito0912@gmail.com>
2023-04-02 17:57:34 +0900

    UserImageCardのテスト実装

---
+++ frontend/__test__/components/parts/UserImageCard/index.test.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react'
+import { UserCard } from '~/components/parts/UserImageCard'
+
+const user = {
+  id: '1',
+  name: 'John Doe',
+  serial_number: 1,
+  created_at: new Date(Date.now()).toISOString(),
+  updated_at: new Date(Date.now()).toISOString(),
+}
+
+test('renders user name', () => {
+  const { getByText } = render(<UserCard user={user} />)
+  const userName = getByText(user.name)
+  expect(userName).toBeInTheDocument()
+})
+
+test('renders "名無しさん" if user name is null', () => {
+  const { getByText } = render(<UserCard user={{ ...user, name: null }} />)
+  const userName = getByText('名無しさん')
+  expect(userName).toBeInTheDocument()
+})
+
+test('renders link with correct href', () => {
+  const { getByRole } = render(<UserCard user={user} />)
+  const link = getByRole('link')
+  expect(link).toHaveAttribute('href', `/users/${user.id}`)
+})
