8cb139d616fa91c446b291e10a85e01d951edf31
shion0625 <xkaito0912@gmail.com>
2023-04-06 13:23:03 +0900

    コメントの追加

--- frontend/src/components/parts/ChoiceInputs/index.tsx
+++ frontend/src/components/parts/ChoiceInputs/index.tsx
@@ -4,25 +4,36 @@ import React, { useCallback } from 'react'
 import { useFieldArray, UseFormRegister, Control } from 'react-hook-form'
 import ChoiceInput from '~/components/parts/ChoiceInput'
 
+// ChoiceInputコンポーネントのプロパティの型定義
 type ChoiceInputsProps = {
-  targetName: string
-  label: string
-  placeholder?: string
-  register: UseFormRegister<any>
-  control: Control<any>
-  type?: React.HTMLInputTypeAttribute | 'text'
+  targetName: string // フォームの対象となるname属性の値
+  label: string // ラベル表示用の文字列
+  placeholder?: string // ヒント表示用の文字列
+  register: UseFormRegister<any> // フォームのregister関数
+  control: Control<any> // フォームのcontrolオブジェクト
+  type?: React.HTMLInputTypeAttribute // 入力欄のtype属性の値
 }
 
-const ChoiceInputs: React.FC<ChoiceInputsProps> = ({ targetName, label, placeholder, register, control, type }) => {
+const ChoiceInputs: React.FC<ChoiceInputsProps> = ({
+  targetName,
+  label,
+  placeholder,
+  register,
+  control,
+  type = 'text',
+}) => {
+  // useFieldArrayフックを使用して、選択肢の配列を制御する
   const { fields, remove, append } = useFieldArray({
     control,
     name: targetName,
   })
 
+  // 選択肢を追加するためのコールバック関数
   const addChoiceItem = useCallback(() => {
     append('')
   }, [append])
 
+  // 選択肢を削除するためのコールバック関数
   const removeChoiceItem = useCallback(
     (index: number) => {
       remove(index)
@@ -45,6 +56,7 @@ const ChoiceInputs: React.FC<ChoiceInputsProps> = ({ targetName, label, placehol
           />
         )
       })}
+      {/* 選択肢を追加するためのボタン */}
       <IconButton aria-label='add' onClick={addChoiceItem}>
         <AddIcon />
       </IconButton>
