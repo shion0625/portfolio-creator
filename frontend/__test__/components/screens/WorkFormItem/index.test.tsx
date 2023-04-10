ff33b9b28a7f9eaab08c7b601b7f33e5b04327dd
shion0625 <xkaito0912@gmail.com>
2023-04-10 22:43:52 +0900

    WorkFormItemのテストコードの実装

---
+++ frontend/__test__/components/screens/WorkFormItem/index.test.tsx
@@ -0,0 +1,91 @@
+import { fireEvent, render } from '@testing-library/react'
+import React from 'react'
+import WorkFormItem from '~/components/screens/WorkFormItem'
+import { useForm } from 'react-hook-form'
+import { WorkFormInput } from '~/models/Work'
+import { WorkFormContext } from '~/stores/workForm';
+
+jest.mock('~/components/parts/ChoiceInputs', () => ({
+  __esModule: true,
+  default: () => <div>Mock ChoiceInputs</div>,
+}))
+
+describe('WorkFormItem', () => {
+  const defaultValues = {
+    works: [{
+      id: '1',
+      title: '',
+      user_id: 'user1',
+    }]
+  }
+
+  const renderComponent = (defaultValues: WorkFormInput) => {
+    const Wrapper = ({ children }: any) => {
+      const methods = useForm({ defaultValues, mode: 'onBlur',})
+      return children(methods)
+    }
+    return render(<Wrapper>{(methods: any) => {
+    const mockContext = {
+      register: methods.register,
+      control: methods.control,
+      errors: methods.formState.errors,
+      workIndex: 0
+    }
+      return (
+          <WorkFormContext.Provider value={mockContext}>
+
+          <WorkFormItem />
+        </WorkFormContext.Provider>
+
+      )
+    }}</Wrapper>)
+  }
+
+  it('should render all fields and error messages', () => {
+    const { getByLabelText, queryByLabelText, getByText } = renderComponent(defaultValues)
+
+    expect(getByLabelText('タイトル')).toBeInTheDocument()
+    expect(getByLabelText('概要')).toBeInTheDocument()
+    expect(getByLabelText('開発期間')).toBeInTheDocument()
+    expect(getByLabelText('開発人数')).toBeInTheDocument()
+    //ChoiceInputsはモック化（複数の入力があるもの）
+    expect(queryByLabelText('開発言語・技術')).not.toBeInTheDocument()
+    expect(getByLabelText('役割')).toBeInTheDocument()
+    //ChoiceInputsはモック化（複数の入力があるもの）
+    expect(queryByLabelText('URL')).not.toBeInTheDocument()
+    expect(getByLabelText('小話')).toBeInTheDocument()
+    expect(getByLabelText('画像')).toBeInTheDocument()
+  })
+
+  it('should not display error message when input is valid', () => {
+  const { queryByText, getByLabelText } = renderComponent(defaultValues)
+
+  const titleInput = getByLabelText('タイトル')
+    fireEvent.change(titleInput, { target: { value: 'Valid Title' } })
+    fireEvent.blur(titleInput) // フォームからフォーカスを外す
+
+  expect(queryByText('⚠ タイトルを入力してください')).not.toBeInTheDocument()
+
+    const numberOfPeopleInput = getByLabelText('開発人数')
+    fireEvent.change(numberOfPeopleInput, { target: { value: 10 } })
+    fireEvent.blur(numberOfPeopleInput) // フォームからフォーカスを外す
+
+  expect(queryByText('⚠ 1人以上100人以下で入力してください')).not.toBeInTheDocument()
+  })
+
+    it('should display error message when input is invalid', () => {
+  const { queryByText, getByLabelText } = renderComponent(defaultValues)
+
+  const titleInput = getByLabelText('タイトル')
+    fireEvent.change(titleInput, { target: { value: '' } })
+    fireEvent.blur(titleInput) // フォームからフォーカスを外す
+
+  expect(queryByText('⚠ タイトルを入力してください')).not.toBeInTheDocument()
+
+    const numberOfPeopleInput = getByLabelText('開発人数')
+    fireEvent.change(numberOfPeopleInput, { target: { value: 10000 } })
+    fireEvent.blur(numberOfPeopleInput) // フォームからフォーカスを外す
+
+  expect(queryByText('⚠ 1人以上100人以下で入力してください')).not.toBeInTheDocument()
+})
+})
